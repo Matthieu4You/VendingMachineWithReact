f0a0fa2cc5b3566cf687c796555fdfe00cec3560
MattuZhou <42333675+Matthieu4You@users.noreply.github.com>
2022-08-05 11:13:33 -0400

    add src

---
+++ src/Components/Body.js
@@ -0,0 +1,10 @@
+const {Component} = require("react");
+
+class Body extends Component {
+    render() {
+        return <div className="body">
+
+        </div>
+    }
+}
+export default Body;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+import Money from "./Components/Money";
+import VendingMachine from "./Components/VendingMachine";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Header/>
+      <VendingMachine/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
